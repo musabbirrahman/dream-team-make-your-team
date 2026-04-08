c00fb9c8b137cdbb34987f5c87a9e624ba7363dc
musabbirrahman <musabbirrahman490@gmail.com>
2026-04-08 18:06:06 +0600

    added toggle button function

--- src/Components/players/Players.jsx
+++ src/Components/players/Players.jsx
@@ -1,16 +1,17 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import AvailablePlayers from './AvailablePlayers/AvailablePlayers';
 
 const Players = ({fetchPlayers}) => {
     const players = use(fetchPlayers)
+    const [selectedButton, setSelectedButton] = useState(true)
     return (
         <div className=' md:container mx-auto w-[90%] my-[50px]'>
 
             <div className='flex justify-between items-center mb-5'>
                 <h1 className='md:ml-10 font-bold md:text-2xl'>Available Players</h1>
                 <div className='flex'>
-                    <button className='btn bg-[#E7FE29] rounded-l-xl rounded-r-none'>Available</button>
-                    <button className='btn md:mr-10 rounded-r-xl rounded-l-none'>Selected</button>
+                    <button onClick={()=> (setSelectedButton(!selectedButton))} className={`btn ${selectedButton ? 'bg-[#E7FE29]' : 'text-[#13131371]'}  rounded-l-xl rounded-r-none`}>Available</button>
+                    <button onClick={()=> (setSelectedButton(!selectedButton))} className={`btn ${!selectedButton ? 'bg-[#E7FE29]' : 'text-[#13131371]'} md:mr-10 rounded-r-xl rounded-l-none`}>Selected</button>
                 </div>
             </div>
             
