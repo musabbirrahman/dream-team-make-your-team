bdf3ccd507afd8f678b2eab729e38bacb654e569
musabbirrahman <musabbirrahman490@gmail.com>
2026-04-08 18:11:35 +0600

    added dynamic text to change according to toggle button

--- src/Components/players/Players.jsx
+++ src/Components/players/Players.jsx
@@ -8,7 +8,7 @@ const Players = ({fetchPlayers}) => {
         <div className=' md:container mx-auto w-[90%] my-[50px]'>
 
             <div className='flex justify-between items-center mb-5'>
-                <h1 className='md:ml-10 font-bold md:text-2xl'>Available Players</h1>
+                {selectedButton ? <h1 className='md:ml-10 font-bold md:text-2xl'>Available Players</h1> : <h1 className='md:ml-10 font-bold md:text-2xl'>Selected Players</h1>}
                 <div className='flex'>
                     <button onClick={()=> (setSelectedButton(!selectedButton))} className={`btn ${selectedButton ? 'bg-[#E7FE29]' : 'text-[#13131371]'}  rounded-l-xl rounded-r-none`}>Available</button>
                     <button onClick={()=> (setSelectedButton(!selectedButton))} className={`btn ${!selectedButton ? 'bg-[#E7FE29]' : 'text-[#13131371]'} md:mr-10 rounded-r-xl rounded-l-none`}>Selected</button>
